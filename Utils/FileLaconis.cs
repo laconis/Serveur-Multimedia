1c9f13e56b36ed4ff3d70c28aef132f21c2efdad
laconis <laconis.dev@gmail.com>
2026-03-09 16:30:18 +0100

    Update FileLaconis.cs
    Add   getCountFolder;

--- Utils/FileLaconis.cs
+++ Utils/FileLaconis.cs
@@ -2,7 +2,12 @@
 
 class FileLaconis{
 
-   
+   /*
+   *Retourne le nombre des dossiers d'un repertoire. 
+   */
+   public static function getCountFolder($dir){
+      return count(glob($dir),GLOB_ONLYDIR);
+   }
 
 
   
