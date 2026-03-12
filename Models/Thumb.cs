520db896b626a46caa19eefba0b9da4f0b2bd1e6
laconis <laconis.dev@gmail.com>
2026-03-12 10:13:28 +0100

    Add GenerateThumb

--- Models/Thumb.cs
+++ Models/Thumb.cs
@@ -1,6 +1,25 @@
 public class Thumb {
 
-  
+  public static bool GenerateThumb(string videoPath,string thumbPath){
+
+    var ffmpegPath = @"C:\\ffmpeg\\bin\\ffmpeg.exe";
+
+    // init 5 secondes
+    var args = $"-ss 00:00:05 -i \" {videoPath} \" -frames:1 -q:2 2 "\{thumbPath} \" -y";
+
+    var psi = new ProcessStartInfo{
+         FileName = ffmpegPath,
+         Arguments = args,
+        RedirrectStandardError = true,
+      RedirrectStandardError = true,
+      CreateNoWindow = true
+    };
+
+    using var process = Process.Start(psi);
+    process!.WaitForExit();
+    return process.ExitCode == 0 && File.Exist(thumbPath)
+    
+  }
 
   
 
