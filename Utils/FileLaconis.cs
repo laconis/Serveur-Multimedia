870eb77f201c69a907d3a017d4931e0b5cd5c05f
laconis <laconis.dev@gmail.com>
2026-03-12 12:35:22 +0100

    Update FileLaconis.cs

--- Utils/FileLaconis.cs
+++ Utils/FileLaconis.cs
@@ -9,6 +9,18 @@ class FileLaconis{
       return count(glob($dir),GLOB_ONLYDIR);
    }
 
+   public static void getFileRecurive(){
+
+   }
+
+   /*
+   * Retourne une liste de fichiers selon le pattern des extensions
+   */
+   public static void getFileRecurive(List<string> patternExtension){
+      
+   }
+
+   
 
   
 }
