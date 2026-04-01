263c4309b93e259c508977fd249ea9f42ea4dd20
laconis <laconis.dev@gmail.com>
2026-04-01 15:49:13 +0200

    Update FileLaconis.cs

--- Utils/FileLaconis.cs
+++ Utils/FileLaconis.cs
@@ -20,6 +20,29 @@ class FileLaconis{
       
    }
 
+   /*
+   * Retourne les 16 premiers bytes du fichier
+   */
+   public static byte[] GetFileHeader(string filePath, int headerSize = 16)
+   {
+       using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+       {
+           byte[] header = new byte[headerSize];
+           fs.Read(header, 0, headerSize);
+           return header;
+       }
+   }
+
+   /*
+   * Retourne les 5 (default) premieres lignes du fichier
+   */
+      public static string GetTextHeader(string filePath, int lines = 5)
+   {
+       return string.Join("\n", File.ReadLines(filePath).Take(lines));
+   }
+      
+
+   
    
 
   
